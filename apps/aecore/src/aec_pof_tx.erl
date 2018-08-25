144d25e8dbf974db27078754d3447b160d1ab6b9
zp-sd <zp.software.development@gmail.com>
2018-08-25 15:17:24 +0200

    Fix behaviour and refactor

--- apps/aecore/src/aec_pof_tx.erl
+++ apps/aecore/src/aec_pof_tx.erl
@@ -1,71 +1,88 @@
-%%%-------------------------------------------------------------------
-%%% @copyright (C) 2018, Aeternity
+%%%=============================================================================
+%%% @copyright 2018, Aeternity Anstalt
 %%% @doc
-%%%
+%%%    Module defining the Proof of Fraud transaction
 %%% @end
-%%%-------------------------------------------------------------------
--module(aec_pof_tx).
+%%%=============================================================================
 
-%% API
--export([new/1,
-    type/0,
-    fee/1,
-    ttl/1,
-    nonce/1,
-    origin/1,
-    header/1,
-    double_header/1,
-    offender/1,
-    reporter/1,
-    check/5,
-    process/5,
-    signers/2,
-    serialization_template/1,
-    serialize/1,
-    deserialize/2,
-    for_client/1
-]).
+-module(aec_pof_tx).
 
 -behavior(aetx).
 
--include("blocks.hrl").
+%% Behavior API
+-export([new/1,
+         type/0,
+         fee/1,
+         ttl/1,
+         nonce/1,
+         origin/1,
+         check/5,
+         process/6,
+         signers/2,
+         serialization_template/1,
+         serialize/1,
+         deserialize/2,
+         for_client/1
+        ]).
+
+%% Getters
+-export([header/1,
+         fraud_header/1,
+         offender/1,
+         reporter/1]).
+
+%%%===================================================================
+%%% Types
+%%%===================================================================
 
 -define(POF_TX_VSN, 1).
 -define(POF_TX_TYPE, pof_tx).
 
 -record(pof_tx, {
-    reporter                  :: aec_id:id(),
-    offender                  :: aec_id:id(),
-    header                    :: aec_id:headers(),
-    double_header             :: aec_id:headers(),
-    fee       = 0             :: non_neg_integer(),
-    ttl       = 0             :: aetx:tx_ttl(),
-    nonce     = 0             :: non_neg_integer()}).
+          reporter     :: aec_id:id(),
+          offender     :: aec_id:id(),
+          header       :: aec_id:headers(),
+          fraud_header :: aec_id:headers(),
+          fee   = 0    :: non_neg_integer(),
+          ttl   = 0    :: aetx:tx_ttl(),
+          nonce = 0    :: non_neg_integer()}).
 
 -opaque tx() :: #pof_tx{}.
 
 -export_type([tx/0]).
 
+%%%===================================================================
+%%% Behaviour API
+%%%===================================================================
+
 -spec new(map()) -> {ok, aetx:tx()}.
-new(#{reporter := Reporter,
-      offender := Offender,
-      header := Header,
-      double_header := DoubleHeader,
-      fee := Fee,
-      nonce := Nonce} = Args) when
-    is_integer(Nonce), Nonce >= 0,
-    is_integer(Fee), Fee >= 0 ->
+new(#{reporter     := Reporter,
+      offender     := Offender,
+      header       := Header,
+      fraud_header := FraudHeader,
+      fee          := Fee,
+      nonce        := Nonce} = Args) when is_integer(Nonce), Nonce >= 0,
+                                          is_integer(Fee), Fee >= 0,
+                                          is_binary(Header),
+                                          is_binary(FraudHeader) ->
     assert_reporter(Reporter),
-    Tx = #pof_tx{reporter = Reporter,
-                 header = Header,
-                 double_header = DoubleHeader,
-                 offender = Offender,
-                 fee = Fee,
-                 ttl = maps:get(ttl, Args, 0),
-                 nonce = Nonce},
+    assert_offender(Offender),
+    Tx = #pof_tx{reporter     = Reporter,
+                 offender     = Offender,
+                 header       = Header,
+                 fraud_header = FraudHeader,
+                 fee          = Fee,
+                 ttl          = maps:get(ttl, Args, 0),
+                 nonce        = Nonce},
     {ok, aetx:new(?MODULE, Tx)}.
 
 assert_reporter(Id) ->
+    case aec_id:specialize_type(Id) of
+        account -> ok;
+        Other   -> error({illegal_id_type, Other})
+    end.
+
+assert_offender(Id) ->
     case aec_id:specialize_type(Id) of
         account -> ok;
         Other   -> error({illegal_id_type, Other})
@@ -91,35 +108,12 @@ nonce(#pof_tx{nonce = Nonce}) ->
 origin(#pof_tx{} = Tx) ->
     reporter_pubkey(Tx).
 
--spec reporter(tx()) -> aec_id:id().
-reporter(#pof_tx{reporter = Reporter}) ->
-    Reporter.
-
--spec offender(tx()) -> aec_id:id().
-offender(#pof_tx{offender = Offender}) ->
-    Offender.
-
--spec header(tx()) -> aec_id:header().
-header(#pof_tx{header = Header}) ->
-    Header.
-
--spec double_header(tx()) -> aec_id:header().
-double_header(#pof_tx{double_header = DoubleHeader}) ->
-    DoubleHeader.
-
--spec reporter_pubkey(tx()) -> aec_keys:pubkey().
-reporter_pubkey(#pof_tx{reporter = Reporter}) ->
-    aec_id:specialize(Reporter, account).
-
-resolve_reporter(#pof_tx{reporter = Reporter}, _Trees) ->
-    {account,  RecipientPubkey} = aec_id:specialize(Reporter),
-    {ok, RecipientPubkey}.
-
 -spec check(tx(), aetx:tx_context(), aec_trees:trees(), aec_blocks:height(), non_neg_integer()) ->
-    {ok, aec_trees:trees()} | {error, term()}.
-check(#pof_tx{} = PoFTx, _Context, Trees, Height, _ConsensusVersion) ->
-    Checks = [], %% TODO: write check to compare offending and local header
-    case aeu_validation:run(Checks, [PoFTx, Trees, Height]) of
+                   {ok, aec_trees:trees()} | {error, term()}.
+check(#pof_tx{} = PoFTx, _Context, Trees, _Height, _ConsensusVersion) ->
+    Checks = [fun check_reporter_account/2],
+    %% TODO: write check to compare offending and local header
+    case aeu_validation:run(Checks, [PoFTx, Trees]) of
         ok ->
             case resolve_reporter(PoFTx, Trees) of
                 {ok, RecipientPubkey} ->
@@ -131,11 +125,9 @@ check(#pof_tx{} = PoFTx, _Context, Trees, Height, _ConsensusVersion) ->
             Error
     end.
 
--spec signers(tx(), aec_trees:trees()) -> {ok, [aec_keys:pubkey()]}.
-signers(#pof_tx{} = Tx, _) -> {ok, [reporter_pubkey(Tx)]}.
-
--spec process(tx(), aetx:tx_context(), aec_trees:trees(), aec_blocks:height(), non_neg_integer()) -> {ok, aec_trees:trees()}.
-process(PoFTx, _Context, Trees0, _Height, _ConsensusVersion) ->
+-spec process(tx(), aetx:tx_context(), aec_trees:trees(), aec_blocks:height(),
+              non_neg_integer(), binary() | no_tx_hash) -> {ok, aec_trees:trees()}.
+process(PoFTx, _Context, Trees0, _Height, _ConsensusVersion, _TxHash) ->
 
     {ok, RecipientPubkey} = resolve_reporter(PoFTx, Trees0),
     AccountsTrees0 = aec_trees:accounts(Trees0),
@@ -150,72 +142,110 @@ process(PoFTx, _Context, Trees0, _Height, _ConsensusVersion) ->
     Trees = aec_trees:set_accounts(Trees0, AccountsTrees1),
     {ok, Trees}.
 
+-spec signers(tx(), aec_trees:trees()) -> {ok, [aec_keys:pubkey()]}.
+signers(#pof_tx{} = Tx, _) -> {ok, [reporter_pubkey(Tx)]}.
 serialize(#pof_tx{
-    reporter = Reporter,
-    offender = Offender,
-    header = Header,
-    double_header = DoubleHeader,
-    fee = Fee,
-    ttl = TTL,
-    nonce = Nonce}) ->
+             reporter = Reporter,
+             offender = Offender,
+             header = Header,
+             fraud_header = FraudHeader,
+             fee = Fee,
+             ttl = TTL,
+             nonce = Nonce}) ->
     {version(),
-        [ {reporter, Reporter}
-        , {header, Header}
-        , {double_header, DoubleHeader}
-        , {offender, Offender}
-        , {fee, Fee}
-        , {ttl, TTL}
-        , {nonce, Nonce}
-        ]}.
+     [ {reporter, Reporter}
+     , {header, Header}
+     , {fraud_header, FraudHeader}
+     , {offender, Offender}
+     , {fee, Fee}
+     , {ttl, TTL}
+     , {nonce, Nonce}
+     ]}.
 
 deserialize(?POF_TX_VSN,
-    [ {reporter, Reporter}
-    , {header, Header}
-    , {double_header, DoubleHeader}
-    , {offender, Offender}
-    , {fee, Fee}
-    , {ttl, TTL}
-    , {nonce, Nonce}
-    ]) ->
+            [ {reporter, Reporter}
+            , {header, Header}
+            , {fraud_header, FraudHeader}
+            , {offender, Offender}
+            , {fee, Fee}
+            , {ttl, TTL}
+            , {nonce, Nonce}
+            ]) ->
     %% Asserts
     account = aec_id:specialize_type(Reporter),
     #pof_tx{
-        reporter = Reporter,
-        header = Header,
-        double_header = DoubleHeader,
-        offender = Offender,
-        fee = Fee,
-        ttl = TTL,
-        nonce = Nonce}.
+       reporter = Reporter,
+       header = Header,
+       fraud_header = FraudHeader,
+       offender = Offender,
+       fee = Fee,
+       ttl = TTL,
+       nonce = Nonce}.
 
 serialization_template(?POF_TX_VSN) ->
     [ {reporter, id}
     , {offender, id}
     , {header, binary}               %% TODO: add header tag?
-    , {double_header, binary}  %% TODO: add header tag?
+    , {fraud_header, binary}  %% TODO: add header tag?
     , {fee, int}
     , {ttl, int}
     , {nonce, int}
     ].
 
 for_client(#pof_tx{
-                    header = Header,
-                    double_header = DoubleHeader,
-                    fee = Fee,
-                    ttl = TTL,
-                    nonce = Nonce} = Tx) ->
+              header = Header,
+              fraud_header = FraudHeader,
+              fee = Fee,
+              ttl = TTL,
+              nonce = Nonce} = Tx) ->
     #{<<"reporter">> => aec_base58c:encode(id_hash, reporter(Tx)),
       <<"data_schema">> => <<"SpendTxJSON">>, % swagger schema name
       <<"offender">> => aec_base58c:encode(id_hash, offender(Tx)),
       <<"header">> => Header,
-      <<"double_header">> => DoubleHeader,
+      <<"fraud_header">> => FraudHeader,
       <<"fee">> => Fee,
       <<"ttl">> => TTL,
       <<"nonce">> => Nonce,
       <<"vsn">> => version()}.
 
-%% Internals
+%%%===================================================================
+%%% Getters
+%%%===================================================================
+
+-spec header(tx()) -> aec_id:header().
+header(#pof_tx{header = Header}) ->
+    Header.
+
+-spec fraud_header(tx()) -> aec_id:header().
+fraud_header(#pof_tx{fraud_header = FraudHeader}) ->
+    FraudHeader.
+
+-spec offender(tx()) -> aec_id:id().
+offender(#pof_tx{offender = Offender}) ->
+    Offender.
+
+-spec reporter(tx()) -> aec_id:id().
+reporter(#pof_tx{reporter = Reporter}) ->
+    Reporter.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+-spec reporter_pubkey(tx()) -> aec_keys:pubkey().
+reporter_pubkey(#pof_tx{reporter = Reporter}) ->
+    aec_id:specialize(Reporter, account).
+
+-spec check_reporter_account(tx(), aec_trees:trees())  ->
+                                    ok | {error, term()}.
+check_reporter_account(#pof_tx{fee = Fee, nonce = TxNonce} = Tx,
+                       Trees) ->
+    ReporterPubkey = reporter_pubkey(Tx),
+    aetx_utils:check_account(ReporterPubkey, Trees, TxNonce, Fee).
+
+resolve_reporter(#pof_tx{reporter = Reporter}, _Trees) ->
+    {account,  RecipientPubkey} = aec_id:specialize(Reporter),
+    {ok, RecipientPubkey}.
 
 version() ->
     ?POF_TX_VSN.
-
